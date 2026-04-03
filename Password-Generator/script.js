08ce858114603b7c73c5e0bf0dfb48a7822cac58
Yogeshmyogi <yogi81660@gmail.com>
2026-04-03 12:58:39 +0530

    Implement password generation functionality

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -1,1 +1,28 @@
+function generatePassword() {
+    let chars = "";
+    
+    if (document.getElementById("uppercase").checked) {
+        chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (document.getElementById("lowercase").checked) {
+        chars += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (document.getElementById("numbers").checked) {
+        chars += "0123456789";
+    }
+    if (document.getElementById("symbols").checked) {
+        chars += "!@#$%^&*()";
+    }
 
+    if (chars === "") {
+        alert("Select at least one option");
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < 12; i++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    document.getElementById("password").value = password;
+}
