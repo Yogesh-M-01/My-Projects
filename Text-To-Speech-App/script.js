2fecfd49c34065d6b91078867013be5f5a49041e
Yogesh M <yogi81660@gmail.com>
2026-05-23 11:25:34 +0530

    Update script.js

--- Text-To-Speech-App/script.js
+++ Text-To-Speech-App/script.js
@@ -1,1 +1,11 @@
+function speakText() {
+    const text = document.getElementById("text").value;
 
+    if (!text) return;
+
+    const speech = new SpeechSynthesisUtterance(text);
+
+    speech.lang = "en-US";
+
+    window.speechSynthesis.speak(speech);
+}
