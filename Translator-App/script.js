f6d78f6b30e5a38d3a1298c62c5b3184115ba453
Yogesh M <yogi81660@gmail.com>
2026-05-24 22:20:03 +0530

    Add async function for text translation

--- Translator-App/script.js
+++ Translator-App/script.js
@@ -1,1 +1,26 @@
+async function translateText() {
 
+    const text = document.getElementById("inputText").value;
+    const target = document.getElementById("language").value;
+
+    const response = await fetch(
+        "https://libretranslate.de/translate",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                q: text,
+                source: "en",
+                target: target,
+                format: "text"
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    document.getElementById("outputText").value =
+        data.translatedText;
+}
