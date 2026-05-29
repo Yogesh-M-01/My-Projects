b515fbe9a05d6018549274414108e42a37d7c4ad
Yogesh M <yogi81660@gmail.com>
2026-05-29 12:31:17 +0530

    Update script.js

--- Resume-Builder/script.js
+++ Resume-Builder/script.js
@@ -1,1 +1,24 @@
+function generateResume(){
 
+    const name =
+    document.getElementById("name").value;
+
+    const role =
+    document.getElementById("role").value;
+
+    const about =
+    document.getElementById("about").value;
+
+    document.getElementById("resume").innerHTML =
+    `
+    <h1>${name}</h1>
+
+    <h3>${role}</h3>
+
+    <hr>
+
+    <h2>About Me</h2>
+
+    <p>${about}</p>
+    `;
+}
