1c08f4238980e52d0ef1ce7b188c2c6e8b7003ad
Yogesh M <yogi81660@gmail.com>
2026-06-04 10:48:41 +0530

    Update style.js

--- Employee-Management-System/style.js
+++ Employee-Management-System/style.js
@@ -1,1 +1,36 @@
+body{
+    background:#0f172a;
+    color:white;
+    font-family:Arial;
+    text-align:center;
+}
 
+.form{
+    margin:20px;
+}
+
+input,select{
+    padding:10px;
+    margin:10px;
+    width:250px;
+}
+
+button{
+    padding:10px 20px;
+    background:#2563eb;
+    color:white;
+    border:none;
+    cursor:pointer;
+}
+
+.card{
+    background:#1e293b;
+    width:400px;
+    margin:10px auto;
+    padding:15px;
+    border-radius:10px;
+}
+
+.delete-btn{
+    background:#dc2626;
+}
